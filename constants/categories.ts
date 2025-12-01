8febd8e1345f4c3e63475b84e3f0cbd4d92be1a8
dboy6598-a11y <dboy6598@gmail.com>
2025-12-01 22:46:16 +0530

    Add predefined expense categories and colors

---
+++ constants/categories.ts
@@ -0,0 +1,9 @@
+export const expenseCategories = [
+  { name: 'Food', color: '#FF5733' },
+  { name: 'Transport', color: '#33C1FF' },
+  { name: 'Utilities', color: '#85FF33' },
+  { name: 'Entertainment', color: '#FF33A0' },
+  { name: 'Health', color: '#FFD633' },
+  { name: 'Education', color: '#33FF57' },
+  { name: 'Others', color: '#FF33F6' }
+];
